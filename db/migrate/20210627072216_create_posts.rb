12c6693ab4beef033ef7fc6334333db464408b34
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-07-04 17:58:26 +0900

    fix: user_idはnullを許可するべきではないので，修正

--- db/migrate/20210627072216_create_posts.rb
+++ db/migrate/20210627072216_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.references :user
+      t.references :user, null: false
       t.string :title, null: false
       t.text :content, null: false
       t.integer :distance, null: false, default: 0
