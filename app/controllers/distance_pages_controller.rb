99f17a45dfc0cf61999759c04f5ee10b2843f4ae
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-07-11 16:24:13 +0900

    feat: 走行距離のグラフをReactで表示

--- app/controllers/distance_pages_controller.rb
+++ app/controllers/distance_pages_controller.rb
@@ -1,8 +1,11 @@
 class DistancePagesController < ApplicationController
   def show
     selected_month = Date.today.all_month
-    @distance_per_month = selected_month.map do |month|
-      [month, current_user.posts.find_by(posted_at: month)&.distance]
+    @current_dates = selected_month.map do |month|
+      month
+    end
+    @current_distances = selected_month.map do |month|
+      current_user.posts.find_by(posted_at: month)&.distance
     end
     @posts_all_month = current_user.posts.map do |post|
       "#{post.posted_at.year}-#{post.posted_at.month}"
--- app/javascript/components/MonthSelect.tsx
+++ app/javascript/components/MonthSelect.tsx
@@ -1,23 +1,43 @@
 import * as React from "react";
+import { Bar } from 'react-chartjs-2';
 
 const { useState } = React;
 
 type MonthSelectProps = {
   monthValues: string[],
   currentMonth: string,
+  currentDates: string[],
+  currentDistances: (number | null)[],
 };
 
-const MonthSelect: React.FC<MonthSelectProps> = ( { monthValues, currentMonth } ) => {
-  const [value, setValue] = useState<string>(currentMonth);
+const MonthSelect: React.FC<MonthSelectProps> = ( { monthValues, currentMonth, currentDates, currentDistances } ) => {
+  const [monthValue, setMonthValue] = useState<string>(currentMonth);
+  const [distanceData, setDistanceData] = useState<(number | null)[]>(currentDistances)
+
+  const data = {
+    labels: currentDates,
+    datasets: [
+      {
+        label: '走行距離(km)',
+        data: distanceData,
+        backgroundColor: 'rgb(75, 192, 192)',
+      }
+    ],
+  };
 
   return (
-    <select value={value} onChange={(e) => setValue(e.target.value)}>
+    <>
+      <select value={monthValue} onChange={(e) => setMonthValue(e.target.value)}>
       {monthValues.map((monthValue) => {
         return (
           <option key={monthValue} value={monthValue}>{monthValue}</option>
         );
       })}
     </select>
+    <Bar
+      data={data}
+    />
+  </>
   );
 };
 
