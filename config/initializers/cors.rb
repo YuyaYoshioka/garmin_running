255cd5d60f4200214a527721aee725044a16e105
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-07-12 01:13:19 +0900

    fix: corsを行えるようにgemを追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://garmin-running.herokuapp.com'
-
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true,
-             expose: %w[access-token uid client expiry]
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
