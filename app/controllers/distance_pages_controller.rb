2aac45edef54db4c8ad54b0eaf2b97532ac081b4
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-07-18 00:52:11 +0900

    fix: 今月のPostが存在しないときもありうるので，最新の年月を取得するように修正

--- app/controllers/distance_pages_controller.rb
+++ app/controllers/distance_pages_controller.rb
@@ -2,7 +2,7 @@ class DistancePagesController < ApplicationController
   def show
     @posts_all_month = current_user.posts.map do |post|
       "#{post.posted_at.year}-#{post.posted_at.month}"
-    end.to_set
-    @current_month = "#{Date.today.year}-#{Date.today.month}"
+    end.to_set.sort
+    @current_month = @posts_all_month.last
   end
 end
